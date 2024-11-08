039530fab74d51f542a7516c795800b28f129727
woogi <47318954+leejw1212@users.noreply.github.com>
2024-11-08 10:49:38 +0900

    Title: 중앙값 구하기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241108/중앙값 구하기/find-the-median.java
+++ 241108/중앙값 구하기/find-the-median.java
@@ -9,28 +9,20 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        int max = a;
-        int min = a;
-
-        if (b > max && c < max) {
-            if (a >= c)
+        if (a > b)
+        {
+            if (c > a)
                 System.out.println(a);
+            else 
+                System.out.println(b);
+        }
+        else if (b > a)
+        {
+            if (c > b)
+                System.out.println(b);
             else
                 System.out.println(c);
         }
-        else if (c > max && b < max) {
-            if (a >= b)
-                System.out.println(a);
-            else
-                System.out.println(b);
-        }
-        else {
-            if (c >= b)
-                System.out.println(c);
-            else
-                System.out.println(b);
-        }
-
 
     }
 }
