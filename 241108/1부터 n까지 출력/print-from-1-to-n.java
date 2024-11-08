7577d034f7a5fe875e574bb8567d0f7e8e6c534f
woogi <47318954+leejw1212@users.noreply.github.com>
2024-11-08 12:57:09 +0900

    Title: 1부터 n까지 출력, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241108/1부터 n까지 출력/print-from-1-to-n.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 1; i<= a; i++)
+            System.out.printf("%d ",i);
+    }
+}
