129444c2ac4fd7a4bd208e52a32f3074f3ddc046
woogi <47318954+leejw1212@users.noreply.github.com>
2024-11-08 10:24:12 +0900

    Title: 최댓값 구하기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241108/최댓값 구하기/maximum-value.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int avg = (a+b+c)/3;
+
+        if (avg > a){
+            if (b>=c)
+                System.out.println(b);
+            else 
+                System.out.println(c);
+        }
+        else if (avg > b){
+            if (a>=c)
+                System.out.println(a);
+            else 
+                System.out.println(c);
+        }
+        else {
+            if (a>=b)
+                System.out.println(a);
+            else 
+                System.out.println(b);
+        }
+    }
+}
