8c9aa258071ac3684c7d0fdfaf7fcd67f6f41d99
woogi <47318954+leejw1212@users.noreply.github.com>
2024-11-11 11:03:30 +0900

    Title: 짝수만 출력 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241111/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int a = sc.nextInt();
+
+        while (b>=a){
+            if (b%2 == 0) System.out.printf("%d ",b);
+            b--;
+        }
+    }
+}
