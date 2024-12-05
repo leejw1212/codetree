bc4980498515b82ad0b225b66e93ddb214ddc559
woogi <47318954+leejw1212@users.noreply.github.com>
2024-12-05 16:00:52 +0900

    Title: 출석 부르기 2, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241205/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,14 @@
+while True:
+    s = int(input())
+
+    if (s == 1):
+        print("John")
+    elif (s == 2):
+        print("Tom")
+    elif (s == 3):
+        print("Paul")
+    elif (s == 4):
+        print("Sam")
+    else:
+        print("Vacancy")
+        break
