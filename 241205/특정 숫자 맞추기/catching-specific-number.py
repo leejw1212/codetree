f8b6aaf21723ecbf7c552cdb6e6dd9cabcda3df9
woogi <47318954+leejw1212@users.noreply.github.com>
2024-12-05 15:59:15 +0900

    Title: 특정 숫자 맞추기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241205/특정 숫자 맞추기/catching-specific-number.py
@@ -0,0 +1,12 @@
+while True:
+    s = int(input())
+
+    if (s == 25):
+        print("Good")
+        break
+    
+    elif (s < 25):
+        print("Higher")
+    
+    else:
+        print("Lower")
