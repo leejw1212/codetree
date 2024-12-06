caa3c7861dfee52843e4d1a604a7401fcd00dd9c
woogi <47318954+leejw1212@users.noreply.github.com>
2024-12-06 13:04:36 +0900

    Title: ab 사이에 있는 c, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241206/ab 사이에 있는 c/c-between-a-and-b.py
@@ -0,0 +1,15 @@
+a,b,c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+
+check = False
+
+for i in range(a,b+1):
+    if i % c == 0:
+        check = True
+
+if check:
+    print("YES")
+else:
+    print("No")
